2174f6f97feb9e80d71839dba64d52ef1fe217e6
anas4444 <anas.elabed@etudiant-fst.utm.tn>
2021-11-27 15:50:51 +0100

    changes

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+from django.db.models.enums import TextChoices
 # Create your models here.
+
+class Absence(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    justification = models.ImageField(upload_to='pdf', null=True, blank=True, default='')
+    valid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Employee Absent on day {self.date}"
+
+class Message(models.Model):
+    class MessageTypes(models.TextChoices):
+        Warning = 'WARNING'
+        Promotion = 'PROMOTION'
+        Fired = 'FIRED'
+
+    type = models.CharField(
+        max_length=9,
+        choices=MessageTypes.choices,
+        default=MessageTypes.Warning,
+    )
+    content = models.TextField()
+    creationDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} Message"
+
+class Employee(models.Model):
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+    grade = models.IntegerField(default=1)
+    monthlyScore = models.IntegerField(default=30)
+    absenceCollection = models.ForeignKey(Absence, on_delete=models.CASCADE, null=True, blank=True, default='')
+    messageCollection = models.ForeignKey(Message, on_delete=models.CASCADE, null=True, blank=True, default='')
+    inscriptionDate = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Employee {self.firstName.title()} {self.lastName.title()}"
