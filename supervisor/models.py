bafb877232019fdaf46709ce600d86eec5de0904
TahmKillerB <mohamado.ezedino@gmail.com>
2021-11-27 10:10:08 +0100

    Changed message types from string to enum

--- supervisor/models.py
+++ supervisor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.enums import TextChoices
 # Create your models here.
 
 class Absence(models.Model):
@@ -10,8 +11,17 @@ class Absence(models.Model):
         return f"Employee Absent on day {self.date}"
 
 class Message(models.Model):
-    type = models.CharField(max_length=100)
-    content = models.CharField(max_length=1000)
+    class MessageTypes(models.TextChoices):
+        Warning = 'WARNING'
+        Promotion = 'PROMOTION'
+        Fired = 'FIRED'
+
+    type = models.CharField(
+        max_length=9,
+        choices=MessageTypes.choices,
+        default=MessageTypes.Warning,
+    )
+    content = models.TextField()
     creationDate = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -27,6 +37,7 @@ class Employee(models.Model):
     absenceCollection = models.ForeignKey(Absence, on_delete=models.CASCADE, null=True, blank=True, default='')
     messageCollection = models.ForeignKey(Message, on_delete=models.CASCADE, null=True, blank=True, default='')
     inscriptionDate = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"Employee {self.firstName.title()} {self.lastName.title()}"
