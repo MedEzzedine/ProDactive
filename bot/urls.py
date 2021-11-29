c6e9868aa2d3572180f41eab229393d17aa71666
Anas <anas.elabed@etudiant-fst.utm.tn>
2021-11-29 10:52:44 +0100

    changes

--- bot/urls.py
+++ bot/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("employee/", EmployeeAPIView.as_view()),
+    #path("employee/", EmployeeAPIView.as_view()),
+    path("employee/", get_allEmployees()),
 ]
--- bot/views.py
+++ bot/views.py
@@ -6,6 +6,8 @@ from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
+
 #import requests
 
 # Create your views here.
@@ -24,6 +26,12 @@ class EmployeeAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+api_view(["GET"])
+def get_allEmployees(request):
+    employee = Employee.objects.all()
+    serializer = EmployeeSerializer(employee, many=True)
+    return Response(serializer.data)
 
 """def collectData(request):
     response = requests.get("https://g7-prodactive.herokuapp.com/test/")"""
