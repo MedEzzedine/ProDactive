3d805f90e2ca523b889be96da1cf7b858c00fc65
TahmKillerB <mohamado.ezedino@gmail.com>
2021-12-14 13:15:26 +0100

    final

--- bot/views.py
+++ bot/views.py
@@ -4,30 +4,13 @@ from django.shortcuts import render
 from rest_framework.serializers import Serializer
 from .models import *
 from .serializers import *
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view  
 
-#import requests
+import requests
 
 # Create your views here.
-
-"""class EmployeeAPIView(APIView):
-
-    def get(self, request):
-        employee = Employee.objects.all()
-        serializer = EmployeeSerializer(employee, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = EmployeeSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)"""
-
 
 @api_view(["GET"])
 def allEmployees(request):
@@ -73,6 +56,8 @@ def getAbsentEmployees(request):
         if len(absence_list)!=0:
             json[emp.id] = absence_list
     return Response(json)
+
+    
 #Employee#
 @api_view(["GET"])
 def checkInbox(request,id):
